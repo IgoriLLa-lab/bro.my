645a9f2e513d417df9c8f97ad2df33abd0dcb430
Igor <crocusik@mail.ru>
2023-06-04 21:53:38 +0500

    LikeDislikeController.php : Добавлены методы like и dislike

--- app/Http/Controllers/LikeDislikeController.php
+++ app/Http/Controllers/LikeDislikeController.php
@@ -2,9 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Article;
+use App\Models\LikeDislike;
 use Illuminate\Http\Request;
 
 class LikeDislikeController extends Controller
 {
-    //
+    public function like(Request $request, Article $article): \Illuminate\Http\RedirectResponse
+    {
+        LikeDislike::create([
+            'article_id' => $article->id,
+            'sum' => 1,
+        ]);
+
+        return back();
+    }
+
+    public function dislike(Request $request, Article $article): \Illuminate\Http\RedirectResponse
+    {
+        LikeDislike::create([
+            'article_id' => $article->id,
+            'sum' => -1,
+        ]);
+
+        return back();
+    }
 }
