b37b9c2e9e832408dee1ac35ed788f213e935f61
Igor <crocusik@mail.ru>
2023-06-04 20:52:58 +0500

    ParseLentaRuNewsCommand.php : add handle

--- app/Console/Commands/ParseLentaRuNewsCommand.php
+++ app/Console/Commands/ParseLentaRuNewsCommand.php
@@ -2,7 +2,11 @@
 
 namespace App\Console\Commands;
 
+use App\Models\Article;
+use App\Models\Tag;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Storage;
+use Symfony\Component\Console\Command\Command as CommandAlias;
 
 class ParseLentaRuNewsCommand extends Command
 {
@@ -25,6 +29,37 @@ class ParseLentaRuNewsCommand extends Command
      */
     public function handle()
     {
-        //
+        $url = 'https://lenta.ru/rss/news';
+        $xmlFile = file_get_contents($url);
+        $xmlObject = (array) simplexml_load_string($xmlFile, null, LIBXML_NOCDATA);
+        $jsonFormatData = json_encode($xmlObject);
+        $result = json_decode($jsonFormatData, true);
+        $count = 0;
+
+        foreach ($result['channel']['item'] as $item) {
+            $tag = Tag::firstOrCreate(['name' => $item['category']]);
+
+            if (Article::where('name', $item['title'])->doesntExist()) {
+                $filename = basename($item['enclosure']['@attributes']['url']);
+                $filename = 'images/' . $filename;
+                Storage::disk('local')->put($filename, file_get_contents($item['enclosure']['@attributes']['url']), 'public');
+
+                $article = Article::create([
+                    'name' => $item['title'],
+                    'description' => $item['description'],
+                    'image' => $filename,
+                ]);
+
+                if (isset($tag)) {
+                    $article->tags()->attach($tag);
+                }
+
+                $count++;
+            }
+        }
+
+        $this->info('Добавлено ' . $count . ' новостей');
+
+        return CommandAlias::SUCCESS;
     }
 }
