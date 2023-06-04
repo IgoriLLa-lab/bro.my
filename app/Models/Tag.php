3adad8116db46039bd8947451edd17b49759e8f1
Igor <crocusik@mail.ru>
2023-06-04 20:54:00 +0500

    Tag.php : Добавил fillable и связь с articles

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Tag extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name'];
+
+    public function articles()
+    {
+        return $this->belongsToMany(Article::class, 'article_tag', 'tag_id', 'article_id');
+    }
+
 }
