5973ece115a371eff4a3dc3a8dc32653de0cb233
Igor <crocusik@mail.ru>
2023-06-04 21:51:41 +0500

    ArticleController.php : в метод index добавлен функционал сортировки по рейтингу (как в ТЗ)

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -4,12 +4,18 @@ namespace App\Http\Controllers;
 
 use App\Models\Article;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ArticleController extends Controller
 {
     public function index()
     {
-        $articles = Article::paginate(10); // Получаем список статей с пагинацией
+        $articles = Article::withCount(['likeDislikes as rating' => function ($query) {
+            $query->select(DB::raw('SUM(sum)'));
+        }])
+            ->orderByDesc('rating')
+            ->orderBy('created_at')
+            ->paginate(10);
 
         return view('welcome', compact('articles'));
     }
