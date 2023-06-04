2be11ca9e99fdfd61968ebb2b5fd33b9d8162978
Igor <crocusik@mail.ru>
2023-06-04 19:50:53 +0500

    добавлены поля для таблицы Articles (статьи)

--- database/migrations/2023_06_04_144507_create_articles_table.php
+++ database/migrations/2023_06_04_144507_create_articles_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('articles', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->string('image')->nullable();
+            $table->dateTime('publication_date')->nullable();
             $table->timestamps();
         });
     }
