c6794678a8919907047498fa7ce77bff34481924
Igor <crocusik@mail.ru>
2023-06-04 20:52:02 +0500

    Console\Kernel.php : add schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,6 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
+        $schedule->command('app:parse-lenta-ru-news-command')->daily();
         // $schedule->command('inspire')->hourly();
     }
 
